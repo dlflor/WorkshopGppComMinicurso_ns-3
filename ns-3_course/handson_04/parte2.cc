2d19b082911473e403b728027609dad91b2079e1
dlflor <dlucenaflor@gmail.com>
2018-07-27 11:29:14 -0700

    a

--- ns-3_course/handson_04/parte2.cc
+++ ns-3_course/handson_04/parte2.cc
@@ -1,15 +1,17 @@
 
-
 #include "ns3/object.h"
 #include "ns3/uinteger.h"
 #include "ns3/traced-value.h"
 #include "ns3/traced-callback.h"
 #include "ns3/trace-source-accessor.h"
+
 #include <stdlib.h>
 #include <time.h>
+
 #include <iostream>
 
 using namespace ns3;
+
 
 
 class MyObject : public Object
@@ -27,7 +29,7 @@ public:
       .AddConstructor<MyObject> ()
       .AddTraceSource ("MyInteger",
                        "An integer value to trace.2",
-                       MakeTraceSourceAccessor (&MyObject:meuint),
+                       MakeTraceSourceAccessor (&MyObject::varint),
                        "ns3::TracedValueCallback::Int32");
 
 
@@ -38,16 +40,15 @@ public:
 
   TracedValue<int32_t> varint;
 
-
-
 };
+
 
 void
 intchange (int32_t velho, int32_t novo)
 {
-	if (novo == 5){
-		std::cout << "Traced int " << novo << " from " << velho << std::endl;
-	}
+    if(novo ==5){
+        std::cout << "Traced int " << novo << " from " << velho << std::endl;
+    }
 }
 
 
@@ -55,20 +56,25 @@ int
 main (int argc, char *argv[])
 {   int a;
     int i;
-
+    int j=0;
+    int b=0;
   Ptr<MyObject> myObject = CreateObject<MyObject> ();
 
   // Conectar as sources com as sinks
 
+
   myObject->TraceConnectWithoutContext ("MyInteger", MakeCallback (&intchange));
 
-  srand( (unsigned)time(NULL));
-  for(i=1; i <= 1000; i++) {
-  	a = rand()%100;
-  	 myObject->variant = a;
-  }
 
 
+  srand( (unsigned)time(NULL) );
+  for(i=1 ; i <= 1000 ; i++) {
+        a=rand()%100;
+         myObject->varint= a;
+
+         }
+
+}
 
 
 }
